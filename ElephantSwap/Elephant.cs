40cd17d75a0881fa314d2037c5855a04c463ee8b
mamaker <madhuvasudevan@hotmail.com>
2015-04-22 23:11:34 -0500

    Done!

---
+++ ElephantSwap/Elephant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElephantSwap
+{
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+        public void WhoAmI()
+        {
+            MessageBox.Show("My ears are " + EarSize + " inches tall.",
+            Name + " says…");
+        }
+    }
+}
