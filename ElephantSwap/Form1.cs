a7569430e559f87480b69e58e8bf3e343f17d5a7
mamaker <madhuvasudevan@hotmail.com>
2015-04-23 19:39:43 -0500

    Added Islands

--- ElephantSwap/Form1.cs
+++ ElephantSwap/Form1.cs
@@ -75,5 +75,29 @@ namespace ElephantSwap
             }
             MessageBox.Show(biggestEars.EarSize.ToString());
         }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            int[] index = new int[4];
+            index[0] = 1;
+            index[1] = 3;
+            index[2] = 0;
+            index[3] = 2;
+            string[] islands = new string[4];
+            islands[0] = "Bermuda";
+            islands[1] = "Fiji";
+            islands[2] = "Azores";
+            islands[3] = "Cozumel";
+            int y = 0;
+            int refNum;
+            while (y < 4) {
+                refNum = index[y];
+                result += "\nisland = ";
+                result += islands[refNum];
+                y = y + 1;
+            }
+            MessageBox.Show(result);
+        }
     }
 }
